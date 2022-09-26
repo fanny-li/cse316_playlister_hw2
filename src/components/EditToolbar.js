336184be3c369812e960a0f5d32219f5e2b75a31
Fanny Li <fannyli704@gmail.com>
2022-09-26 00:05:45 -0400

    disabled buttons draft 1

--- src/components/EditToolbar.js
+++ src/components/EditToolbar.js
@@ -8,10 +8,10 @@ export default class EditToolbar extends React.Component {
         let undoClass = "toolbar-button";
         let redoClass = "toolbar-button";
         let closeClass = "toolbar-button";
-        if (canAddSong) addSongClass += " disabled";
-        if (canUndo) undoClass += " disabled";
-        if (canRedo) redoClass += " disabled";
-        if (canClose) closeClass += " disabled";
+        if (!canAddSong) addSongClass = "playlister-button-disabled";
+        if (!canUndo || !canClose) undoClass = "playlister-button-disabled";
+        if (!canRedo || !canClose) redoClass = "playlister-button-disabled";
+        if (!canClose) closeClass = "playlister-button-disabled";
         return (
             <div id="edit-toolbar">
                 <input
--- src/components/SidebarHeading.js
+++ src/components/SidebarHeading.js
@@ -1,17 +1,21 @@
 import React from "react";
 
 export default class SidebarHeading extends React.Component {
+
     handleClick = (event) => {
         const { createNewListCallback } = this.props;
         createNewListCallback();
     };
     render() {
+        const { canAddList } = this.props;
+        let addListClass = "toolbar-button";
+        if (canAddList) { addListClass = "playlister-button-disabled" }
         return (
             <div id="sidebar-heading">
-                <input 
-                    type="button" 
-                    id="add-list-button" 
-                    className="toolbar-button" 
+                <input
+                    type="button"
+                    id="add-list-button"
+                    className={addListClass}
                     onClick={this.handleClick}
                     value="+" />
                 Your Playlists
